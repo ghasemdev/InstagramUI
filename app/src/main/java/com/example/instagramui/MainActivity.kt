9edf2ddc02d3000203b0192149e4fb48450d3790
ghasem <shirdelghasem79@gmail.com>
2021-08-14 15:37:06 +0430

    init commit

---
+++ app/src/main/java/com/example/instagramui/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.instagramui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.instagramui.ui.theme.InstagramUITheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            InstagramUITheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    InstagramUITheme {
+        Greeting("Android")
+    }
+}
